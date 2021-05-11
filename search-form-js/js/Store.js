0d0cb4453c2f0eb6524e41d840c2d179b73936ae
jonghyun <jonghyeon2046@gmail.com>
2021-05-11 16:58:39 +0900

    [UPDATE]검색결과, 상품 검색 메서드 추가

--- search-form-js/js/Store.js
+++ search-form-js/js/Store.js
@@ -2,9 +2,21 @@ const tag = "[store]";
 
 export default class Store {
     constructor(storage) {
-        console.log(tag, "store")
+        console.log(tag, "store");
         if (!storage) throw "no storage";
 
         this.storage = storage;
+
+        //검색결과
+        this.searchKeyword = "";
+        this.searchResult = [];
+    }
+
+    //searchKeyword에 해당하는 상품 검색 메서드
+    search(keyword) {
+        this.searchKeyword = keyword;
+        this.searchResult = this.storage.productData.filter((product) =>
+            product.name.includes(keyword)
+        );
     }
 }
